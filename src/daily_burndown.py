f72effabad1ad6086dfd292c8372faab8c7dcc5f
allen.qin <allen.qin@yeahmobi.com>
2020-07-30 18:04:53 +0800

    Jul data update

--- src/daily_burndown.py
+++ src/daily_burndown.py
@@ -1,3 +1,5 @@
+import math
+
 import matplotlib.pyplot as plt
 import pandas
 
@@ -14,24 +16,30 @@ def main():
     week_target = []
     week_actual = []
     # target list
+    left = target
     for i in range(period):
-        week_target.append(target - target/period * i)
+        period_target = round(target - target / (period -1) * i, 2)
+        week_target.append(period_target if period_target > left else left)
+        left = left - period_target
     # actual list
     personal_data = read_personal_data()
 
     total_km = 0
     for km in personal_data:
-        total_km += km
+        km = str(km).strip()
+        if km == '':
+            continue
+        total_km += round(float(km),2)
         week_actual.append(target - total_km)
 
     plt.figure(facecolor='#A9A9A9')
     plt.axes(facecolor='k')
     plt.axhline(0, 0, target, color='w')
     plt.axvline(0, 0, period, color='w')
-    plt.title('200 km Burn Down for June')
+    plt.title('200 km Burn Down for July')
     plt.plot(week_target)
     plt.plot(week_actual)
-    plt.xlabel('Burn Down by Day')
+    plt.xlabel('Burn Down by Day. Now:{}km'.format(round(total_km, 2)))
     plt.ylabel('GOAL for 200km')
     plt.plot()
     plt.show()
